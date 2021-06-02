b8508daa5ecc3abde28daf51e16a6fa26175e10c
JAy-Parekh <84897316+JAy-Parekh@users.noreply.github.com>
2021-06-03 02:02:51 +0530

    Add files via upload

---
+++ Task4/associativearray.php
@@ -0,0 +1,45 @@
+<?php
+//Associative Array
+//Method 1
+
+$a[0] = 10;
+$a['c'] = "Computer";
+$a['php'] = "Web Development";
+$a[10] = "Ten";
+$a[50] = 50.50;
+
+//Method 2
+
+$a = array(
+    0 => 10,
+    "c" => "Computer",
+    "php" => "Web Development",
+    10 => "Ten",
+    50 => 50.50
+);
+
+//Print value
+echo "C for ".$a['c'];
+echo "<br>PHP is used for ".$a['php'];
+
+//Print Whole array
+foreach ($a as $key => $value) {
+    echo "<br>".$key." = ".$value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
